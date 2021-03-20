d9e88ad8a1176317a57e0f6c1fec8a831a1dc4d0
webaspentity <71076578+webaspentity@users.noreply.github.com>
2021-03-20 21:39:38 +0300

    Update Primes.java

--- Lab1/src/com/evseev/Primes.java
+++ Lab1/src/com/evseev/Primes.java
@@ -4,13 +4,13 @@ public class Primes
 {
 
 
-    public static void main(String[] args) {
+    public static void getPrimes(String[] args) {
         for (int i = 2; i < 100; i++)
-            if (IsPrime(i)) System.out.print(i + "; ");
+            if (isPrime(i)) System.out.print(i + "; ");
     }
 
 
-    public static boolean IsPrime(int n) {
+    private static boolean isPrime(int n) {
         for (int i = 2; i <= Math.sqrt(n); i++)
             if (n % i == 0) return false;
 
